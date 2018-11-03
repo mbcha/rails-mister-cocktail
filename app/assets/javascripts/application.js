e6baf6c3a040329030a542df59a76229579b1dde
Bella Cha <cha.maria.belen@gmail.com>
2018-11-03 12:41:39 +0100

    added jquery to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
 
 import 'bootstrap';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
-import 'bootstrap'
+//= require jquery
+//= require jquery_ujs
+
+import 'bootstrap';
